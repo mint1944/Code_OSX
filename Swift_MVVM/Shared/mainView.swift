193c952a338958ff4ad4419c5993b17750708b2e
MINt <63347610+mint1944@users.noreply.github.com>
2022-09-03 00:27:33 +0800

    修改文件

--- Swift_MVVM/Shared/mainView.swift
+++ Swift_MVVM/Shared/mainView.swift
@@ -8,16 +8,32 @@
 import SwiftUI
 
 struct mainView: View {
-    @StateObject var NewmainViewModel = mainViewModel()
+    @State var Height:CGFloat = 100
     var body: some View {
         NavigationView {
-            List {
-                Text("\(NewmainViewModel.new.name)")
-                Text("\(NewmainViewModel.new.gender)")
-                Text("\(NewmainViewModel.new.age)")
-                Text("\(NewmainViewModel.new.word)")
+            ScrollView(showsIndicators: false) {
+                VStack(spacing: 0) {
+                    ForEach(Range(1...5)) { times in
+                        HStack(spacing: 0) {
+                            Rectangle()
+                                .frame(width: Height, height: Height)
+                            VStack(spacing: 0) {
+                                
+                            }
+                            .frame(height: Height)
+                            .frame(maxWidth: .infinity)
+                            .background(Color.blue)
+                        }
+                        .frame(height: Height)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.gray.brightness(0.3))
+                        .clipShape(RoundedRectangle(cornerRadius: 15, style: .continuous))
+                        .padding(.horizontal)
+                        .padding(.vertical, 5)
+                    }
+                }
             }
-            .navigationTitle("MVVM_Socket")
+            .navigationTitle("Swift 5.0")
         }
     }
 }
