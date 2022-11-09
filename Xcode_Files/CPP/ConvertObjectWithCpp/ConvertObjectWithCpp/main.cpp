595e3bda876815bb9fff95dfecb0a2fd463b1505
MINt <63347610+mint1944@users.noreply.github.com>
2022-11-09 22:23:05 +0800

    update

--- Xcode_Files/CPP/ConvertObjectWithCpp/ConvertObjectWithCpp/main.cpp
+++ Xcode_Files/CPP/ConvertObjectWithCpp/ConvertObjectWithCpp/main.cpp
@@ -1,38 +1,13 @@
-/// 类型转换
-/// int to char
-/// int to char*
-/// int to string
-/// char to int
-/// float to string
-/// float to char*
-/// double to string
-/// double to char*
-
 #include <iostream>
 #include <string.h>
+#include <ctime>
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-    char str[12] = "Number is 1";
-    char str2[13];
-    for(int x = 0; x <= 12; x++) {
-        if (x == 12) {
-            str2[12] = 2;
-            break;
-        }
-        
-        str2[x] = str[x];
-        
-    }
+int main(int argc, char **argv) {
     
-    string x = to_string(2);
-    
-    string s = str2;
-    
-    string c = s + x;
-    
-    cout << c << endl;
+    time_t now = time(0); // 获取系统当前时间
+    tm *cur = localtime(&now); // 将获取的时间转换为结构体
     
     return 0;
 }
