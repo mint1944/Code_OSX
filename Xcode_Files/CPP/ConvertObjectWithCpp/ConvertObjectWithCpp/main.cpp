afad71773ab098e291470a8cd3db52c8c437e8c0
MINt <63347610+mint1944@users.noreply.github.com>
2022-11-14 14:37:48 +0800

    update

--- Xcode_Files/CPP/ConvertObjectWithCpp/ConvertObjectWithCpp/main.cpp
+++ Xcode_Files/CPP/ConvertObjectWithCpp/ConvertObjectWithCpp/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string.h>
 #include <ctime>
 
 using namespace std;
@@ -8,6 +7,6 @@ int main(int argc, char **argv) {
     
     time_t now = time(0); // 获取系统当前时间
     tm *cur = localtime(&now); // 将获取的时间转换为结构体
-    
+    cout << cur->tm_year + 1900 << endl;
     return 0;
 }
